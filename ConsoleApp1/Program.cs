0312cd663b0d5d865cbeda5d2e03dcbcd464a3e4
student <soloxpwnz@mail.ru>
2020-09-11 17:49:57 +0300

    pr1_ex2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 
-namespace ConsoleApp1
+namespace pr1_ex2
 {
   struct Person
   {
+    public enum Genders : int { Male, Female };
     public string firstName;
     public string lastName;
     public int age;
-    public Person(string _firstName, string _lastName, int _age)
+    public Genders gender;
+    public Person(string _firstName, string _lastName, int _age, Genders _gender)
     {
       firstName = _firstName;
-      lastName = _lastName; age = _age;
+      lastName = _lastName; age = _age; gender = _gender;
     }
     public override string ToString()
     {
-      return firstName + " " + lastName + ", age " + age;
+      return firstName + " " + lastName + " ( " + gender + "), age  " + age;
     }
 
   }
@@ -23,7 +25,8 @@ namespace ConsoleApp1
   {
     static void Main(string[] args)
     {
-      Person p = new Person("Tony", "Allen", 32); Console.WriteLine(p);
+      Person p = new Person("Tony", "Allen", 32, Person.Genders.Male);
+      Console.WriteLine(p.ToString());
     }
   }
 }
